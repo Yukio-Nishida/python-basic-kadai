5ee8fca522f294e9f926bbf430564b3a8b37553a
Yukio-Nishida <142161225+Yukio-Nishida@users.noreply.github.com>
2024-03-29 11:57:55 +0900

    Update var_if.py

--- kadai_008/var_if.py
+++ kadai_008/var_if.py
@@ -7,17 +7,17 @@ var = random.randint(0, 50)
 #変数varの値を出力
 print(var)
 
+#3の倍数と5の倍数の両方に該当する場合は「FizzBuzz」と出力
+if var % 15 == 0:
+    print("FizzBuzz")
+
 #変数varの値が3の倍数なら「Fizz」と出力
-if var % 3 == 0:
+elif var % 3 == 0:
     print("Fizz")
 
 #5の倍数なら「Buzz」と出力
 elif var % 5 == 0:
     print("Buzz")
-
-#3の倍数と5の倍数の両方に該当する場合は「FizzBuzz」と出力
-elif var % 15 == 0:
-    print("FiZZBuzz")
 
 #どれにも該当しない場合は変数varの値を出力
 else:
